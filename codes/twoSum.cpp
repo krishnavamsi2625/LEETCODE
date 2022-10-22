b23472f5b810720ddf3e61a652b7a839289059ed
krishnavamsi2625 <krishnavamsi742@gmail.com>
2022-10-22 10:49:12 +0530

    .

--- codes/twoSum.cpp
+++ codes/twoSum.cpp
@@ -1,23 +1,47 @@
+//O(n^2) time complexity
+//class Solution {
+//public:
+//    vector<int> twoSum(vector<int>& nums, int target) {
+//        vector<int> t;
+//        for(int i=0;i<nums.size();i++)
+//        {   
+//            int f=0;
+//            for(int j=i+1;j<nums.size();j++)
+//            {
+//                if(nums[i]+nums[j]==target)
+//                {
+//                  t.push_back(i);
+//                  t.push_back(j);
+//                  f=1;
+//                  break;
+//                }
+//            }
+//            if(f)
+//            break;
+//          }
+//        return t;
+//        
+//    }
+//};
+
+
+//O(n) Time complexity
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> db;
         vector<int> t;
         for(int i=0;i<nums.size();i++)
-        {   
-            int f=0;
-            for(int j=i+1;j<nums.size();j++)
+        {
+            if(db.count(target-nums[i]))
             {
-                if(nums[i]+nums[j]==target)
-                {
-                  t.push_back(i);
-                  t.push_back(j);
-                  f=1;
-                  break;
-                }
+                
+                t.push_back(db[target-nums[i]]);
+                t.push_back(i);
+                return t;
             }
-            if(f)
-            break;
-          }
+            db[nums[i]]=i;
+        }
         return t;
         
     }
